95ff088ad3b2364156cee593102e862f4cae1806
nicoPrasetiawan <nicoprasetiawan28@gmail.com>
2025-06-13 14:27:01 +0700

    test

--- src/EcommerceAppRefactor.java
+++ src/EcommerceAppRefactor.java
@@ -9,13 +9,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class EcommerceAppRefactor {
 
 	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("Running main method...");
+
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
-		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		
 		List<String> pickedItems = Arrays.asList("Cucumber", "Beans", "Brinjal");
+		
+		addToCart(driver, pickedItems);
 
+
+		Thread.sleep(2000);
+		driver.quit();
+
+	}
+	
+	public static void addToCart(WebDriver driver, List<String> pickedItems) {
+		
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		
 		int addCOunt = 0;
 		for (int i = 0; i < products.size(); i++) {
 			String productName = products.get(i).getText().split("-")[0].trim();
@@ -30,10 +45,7 @@ public class EcommerceAppRefactor {
 				;
 			}
 		}
-
-		Thread.sleep(2000);
-		driver.quit();
-
+		
 	}
 
 }
