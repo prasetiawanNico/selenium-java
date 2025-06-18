fb5887fd507596815146a82bd791684b267faaa0
nico <prasetiawannico@gmail.com>
2025-06-18 09:32:09 +0700

    .

--- src/FrameAssignment.java
+++ src/FrameAssignment.java
@@ -1,8 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FrameAssignment {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://the-internet.herokuapp.com/nested_frames");
+		
+		driver.switchTo().frame("frame-top");
+		driver.switchTo().frame("frame-middle");
+		
+		System.out.println(driver.findElement(By.id("content")).getText());
 
 	}
 
